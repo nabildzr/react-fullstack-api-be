e4ec0507fc8dfa9080a86bd30a70b8b9def81cc1
nabildzr <muhammadnbillz@gmail.com>
2025-04-23 07:02:27 +0700

    Refactor PostController to return user data with posts in index, store, show, and update methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function index()
     {
-        return Post::all();
+        return Post::with('user')->latest()->get();
     }
 
     /**
@@ -37,7 +37,7 @@ class PostController extends Controller implements HasMiddleware
 
         $post = $request->user()->posts()->create($fields);
 
-        return $post;
+        return ['post' =>  $post, 'user' => $post->user];
     }
 
     /**
@@ -45,7 +45,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function show(Post $post)
     {
-        return $post;
+        return ['post' => $post, 'user' => $post->user];
     }
 
     /**
@@ -61,7 +61,7 @@ class PostController extends Controller implements HasMiddleware
 
         $post->update($fields);
 
-        return   $post;
+        return ['post' =>  $post, 'user' => $post->user];
     }
 
     /**
